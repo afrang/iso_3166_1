0a73daca35fe126285061eed708718ea4ef326ee
AfrangArt <afrangart@gmail.com>
2025-02-06 01:40:21 +0330

    Update convertorclass.ts

--- convertorclass.ts
+++ convertorclass.ts
@@ -7,4 +7,10 @@ export class GeneralClass {
        
        return country ? country['persian_name'] : isoCode;
      }
+     getPersianNameByLang({isoCode}:any): string | undefined {
+        const country:any = _TvPediaClass.CountryList.find((scountry:any) => scountry.iso_639_1 ==isoCode);
+        
+        return country ? country['iso_639_1_persian'] : isoCode;
+      }
+}
 }
